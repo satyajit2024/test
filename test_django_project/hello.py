74ec007b3058a5d70fc23bf710fcc4c73f6bcaa9
satyajit2024 <satyajit.bariflo@outlook.com>
2024-04-10 12:16:16 +0530

    test

---
+++ test_django_project/hello.py
@@ -0,0 +1,2 @@
+def new():
+    print("Hello")
---
+++ test_django_project/test_app/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+class TestAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'test_app'
+
+    def ready(self):
+        from hello import new
+        new()
